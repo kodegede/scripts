d31c103732554a6807957c15004f1eb65e2cab2a
GAP <96944057+kodegede@users.noreply.github.com>
2022-01-14 23:29:47 +0800

    SC-3 learn chdir

---
+++ python/chdir.py
@@ -0,0 +1,28 @@
+import os
+   
+# dir = os.getcwd()
+# print ('current directory: ', dir)
+
+# os.chdir('../')
+
+# print ('current directory: ', dir)
+
+
+# def current_path():
+#     print(os.getcwd())
+#     print()
+
+
+
+# current_path()
+
+# os.chdir('../')
+
+# current_path()
+
+
+print(os.getcwd())
+
+os.chdir('../')
+
+print(os.getcwd())
