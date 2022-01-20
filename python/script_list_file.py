e5563b576ed4c63b3f1523ec12d56dcde5018975
GAP <96944057+kodegede@users.noreply.github.com>
2022-01-20 23:47:49 +0800

    SC-11 add folder empty checker

--- python/script_list_file.py
+++ python/script_list_file.py
@@ -1,7 +1,12 @@
 # this script receive user's directory input and give list of file in directory as output
+# tested on 2022/01/19
+ 
 import os
 
 dir = input("insert the directory:")
 dir_list = os.listdir(dir)
-print(dir_list)
 
+if len(os.listdir(dir)) == 0:
+    print("Directory is empty")
+else:    
+    print(dir_list)
