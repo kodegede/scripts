f20b2fd93a715cce43479c97f8842087f1324c61
GAP <96944057+kodegede@users.noreply.github.com>
2022-01-22 23:58:38 +0800

    SC-14 continue script os combi

---
+++ python/script_os_combi.py
@@ -0,0 +1,12 @@
+#this script use all of function of os module that I have learned before
+
+import os
+
+print("script os testing:")
+print("a. getcwd")
+print("a. chdir")
+
+dir = input("insert the choice:")
+
+if dir=='a':
+    
