1396a7e853f66f899c6d50696cb02a948f7b1db9
GAP <96944057+kodegede@users.noreply.github.com>
2022-01-23 23:55:10 +0800

    SC-14 continue scripting os

--- python/script_os_combi.py
+++ python/script_os_combi.py
@@ -1,12 +1,17 @@
 #this script use all of function of os module that I have learned before
 
 import os
+from random import choice
 
 print("script os testing:")
 print("a. getcwd")
 print("a. chdir")
 
-dir = input("insert the choice:")
+cho = input("insert the choice:")
+dir = os.getcwd()
 
-if dir=='a':
-    
+
+if cho=='a':
+    print('current working directory is:', dir)
+if cho=='b':  
+    print(os.getcwd())
